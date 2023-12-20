b978c1c9bf76ef62695772834db07dbd917af2ed
TemurEShtemirov <temureshtemirov10@gmail.com>
2023-12-20 21:46:06 +0500

    added html and css and js and added to js speaker

---
+++ app.js
@@ -0,0 +1,17 @@
+const button = document.querySelector(".talk");
+const content = document.querySelector(".content");
+
+function speak(text) {
+  const text_speak = new SpeechSynthesisUtterance(text);
+
+  text_speak.rate = 1;
+  text_speak.volume = 1;
+
+  text_speak.pitch = 1;
+
+  window.speechSynthesis.speak(text_speak);
+}
+
+window.addEventListener("load", () => {
+  speak("Initializing JARVIS ...");
+});
