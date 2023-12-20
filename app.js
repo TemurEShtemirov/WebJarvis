0eae33c242458449ecf248d6c155549cdcabb518
TemurEShtemirov <temureshtemirov10@gmail.com>
2023-12-20 22:21:00 +0500

    added style and finished html

--- app.js
+++ app.js
@@ -12,6 +12,43 @@ function speak(text) {
   window.speechSynthesis.speak(text_speak);
 }
 
+function wishMe() {
+  var day = new Date();
+
+  var hour = day.getHours();
+
+  if (hour >= 0 && hour < 12) {
+    speak("Good Morning Boss ...");
+  } else if (hour < 12 && hour < 17) {
+    speak("Good Afternoon Master...");
+  } else {
+    speak("Good Evening Sir...");
+  }
+}
+
 window.addEventListener("load", () => {
   speak("Initializing JARVIS ...");
+  wishMe();
 });
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.onresult = (event) => {
+
+  const currentIndex = event.resultIndex;
+  const transcript = event.result[currentIndex][0].transcript;
+  content.textContent = transcript;
+
+  takeCommand(textContent.toLowerCase());
+};
+
+button.addEventListener("click", () => {
+  content.textContent = "Listening ...";
+  recognition.start();
+});
+
+function takeCommand() {
+    
+}
